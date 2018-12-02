ae5a8df7d674bf6fe7a384f364f48714e9ffa6f7
jaypatrickdeguzman <jaypatrickdeguzman@gmail.com>
2018-12-01 18:02:44 -0600

    Added cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 app.use(morgan("dev"));
 
 const PORT = process.env.PORT || 1337;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).send("<p>Success</p>");
