128e292d4a1f3c0940a59b4c08b5e6f0e30f9671
jaypatrickdeguzman <jaypatrickdeguzman@gmail.com>
2018-12-01 21:42:06 -0600

    Setting up server structure

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const path = require("path");
+
+const router = require("./router");
+
 const app = express();
 
 app.use(morgan("dev"));
@@ -8,6 +12,7 @@ app.use(morgan("dev"));
 const PORT = process.env.PORT || 1337;
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, "../src/dist")));
 
 app.get("/", (req, res) => {
   res.status(200).send("<p>Success</p>");
---
+++ server/router.js
@@ -0,0 +1,4 @@
+const router = require("express").Router;
+const cors = require(cors());
+
+module.exports = router;
