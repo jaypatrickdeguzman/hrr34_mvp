c2427985722ab17123a5d6d34365814305c35f83
jaypatrickdeguzman <jaypatrickdeguzman@gmail.com>
2018-12-03 10:38:39 -0600

    Seed script written

---
+++ server/seeding/seed.js
@@ -0,0 +1,35 @@
+const faker = require("faker");
+const mongoose = require("mongoose");
+const Profile = require("../database/model");
+
+function rand(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function createProfile() {
+  let count = 0;
+  return Array.from({ length: 1000 }, () => {
+    let id = count;
+    count++;
+    return {
+      _id: id.toString(),
+      username: faker.internet.userName(),
+      password: faker.internet.password(),
+      description: faker.lorem.sentence(),
+      location: faker.address.state,
+      name: `${faker.name.firstName} ${faker.name.lastName}`,
+      age: rand(21, 30),
+      likes: rand(1, 100)
+    };
+  });
+}
+
+const profiles = createProfile();
+
+function seedDB() {
+  Profile.create(profiles)
+    .then(() => mongoose.connection.close())
+    .catch(err => console.error(err));
+}
+
+seedDB();
