2de346de5c6f1e030250b0c7cb8c18b6fa17d5d7
jaypatrickdeguzman <jaypatrickdeguzman@gmail.com>
2018-12-03 15:55:13 -0600

    Only importing useState in FormInput.js

--- src/components/hooks/FormInput.js
+++ src/components/hooks/FormInput.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 export default function useFormInput(initialValue) {
   const [value, setValue] = useState(initialValue);
