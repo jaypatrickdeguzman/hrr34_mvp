3cd176b551cf8563b7aa4877baa798bc4479775e
jaypatrickdeguzman <jaypatrickdeguzman@gmail.com>
2018-12-04 10:27:33 -0600

    More changes

--- src/lib/signin.js
+++ src/lib/signin.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Change base URL
 export default axios.create({
-  baseURL: "http://localhost:1337/api",
+  baseURL: "https://jay-hrr34-mvp.herokuapp.com/api",
   headers: {
     "Access-Control-Allow-Origin": "*"
   }
